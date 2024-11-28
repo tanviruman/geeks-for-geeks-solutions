8eca0cbbe7fa50c0c3cbc9e7cb958adb228e203f
Tanvir  Ahmed <ruman777ahmed@gmail.com>
2024-11-28 11:18:06 +0600

    ok

---
+++ CPP Practice/Learn to Comment.cpp
@@ -0,0 +1,16 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    // Comment the part that outputs b
+    cout << a << " ";
+    //cout << b << " ";
+    cout << c << " ";
+    cout << endl;
+
+    return 0;
+}
