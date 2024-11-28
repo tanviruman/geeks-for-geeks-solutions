040f82fe2fc3f2119716ac0e164cd5afaa810e7b
Tanvir  Ahmed <ruman777ahmed@gmail.com>
2024-11-28 11:15:27 +0600

    ok

---
+++ CPP Practice/Taking input.cpp
@@ -0,0 +1,15 @@
+
+
+
+class Solution {
+  public:
+  int a;
+  string b;
+    void inputData() {
+        // code here
+      
+        cin>>a;
+        cin>>b;
+        cout<<a<<" "<<b<<endl;
+    }
+};
